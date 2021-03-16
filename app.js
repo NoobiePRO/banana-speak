9796a4a798f53c447948fa7143e5139e2fc8e218
Sarthak Sharma <sarthaksharma_ma@srmuniv.edu.in>
2021-03-16 23:56:13 +0530

    taking input and console.

--- app.js
+++ app.js
@@ -1,12 +1,44 @@
-console.log("Script is working from diffrent file.")
+// console.log("Script is working from diffrent file.")
 
 
 
 
 
-// #part1
+// // #part1
 
-var x=prompt("gimme a usernme.")
+// var x=prompt("gimme a usernme.")
 
-alert("this script works!!"+x)
+// alert("this script works!!"+x)
+
+
+
+
+
+var button_translate = document.querySelector("#btn-translate")
+// console.log(button_translate)
+
+var txtinput = document.querySelector("#txt-input")
+
+console.log(txtinput)
+
+function clickEventHandler(){
+    console.log("clicked!")
+    console.log("input", txtinput.value);
+}
+
+button_translate.addEventListener("click", clickEventHandler)
+
+
+
+
+
+
+
+
+
+//google hack= document.body.style.backgroundColor = "green"
+
+//browser events in javascript
+
+//eloquent javascript
 

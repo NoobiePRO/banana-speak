c611c43d63b39585d27ef3dec92b641c2a4d92e6
Sarthak Sharma <sarthaksharma_ma@srmuniv.edu.in>
2021-03-17 22:42:16 +0530

    input output processing done.

--- app.js
+++ app.js
@@ -18,17 +18,25 @@ var button_translate = document.querySelector("#btn-translate")
 // console.log(button_translate)
 
 var txtinput = document.querySelector("#txt-input")
+// console.log(txtinput)
 
-console.log(txtinput)
+var outputDiv = document.querySelector("#output" );
+// outputDiv.innerText = "Sarthak"
 
 function clickEventHandler(){
-    console.log("clicked!")
-    console.log("input", txtinput.value);
+    // console.log("clicked!")
+    // console.log("input", txtinput.value);
+
+    outputDiv.innerText = "ajsjsjsjsjsjsj "+ txtinput.value
 }
 
 button_translate.addEventListener("click", clickEventHandler)
 
 
+//linked in== document.queryselectorAll(".artdeco-button--secondary")
+//   .forEach(item => setTimeout(item.click(),Math.random=1000)
+
+//queryselector--blog,read,quiz   css_selector
 
 
 
